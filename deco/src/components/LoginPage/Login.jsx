1b984a6ab7445c91257bf72e99fcf9584562b7f8
Jayoung Hong <gripix324@gmail.com>
2023-03-13 18:00:11 +0900

    [Feat] 회원이 아니면 회원가입으로 이동시켜주는 부분 제작

--- deco/src/components/LoginPage/Login.jsx
+++ deco/src/components/LoginPage/Login.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import WelcomeInfo from "@/components/Common/WelcomeInfo/WelcomeInfo";
 import FormInput from "@/components/Common/FormInput/FormInput";
+import SubmitButton from "@/components/Common/SubmitButton/SubmitButton";
 import classes from "./Login.module.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className={classes.layout}>
+    <div className={classes.container}>
       <WelcomeInfo />
       <FormInput />
       <FormInput />
+      <SubmitButton />
+      <p className={classes.info}>
+        아직 회원이 아니시라면{" "}
+        <Link to="./" className={classes.toSignUp}>
+          회원가입
+        </Link>
+      </p>
     </div>
   );
 };
