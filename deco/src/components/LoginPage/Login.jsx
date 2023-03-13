9a01a3b5e919c99910221e0f67c14aa332fd9abc
Jayoung Hong <gripix324@gmail.com>
2023-03-13 11:44:05 +0900

    [Setting] 로그인 페이지 - Login.jsx 생성 - Login.module.css 생성 - LoginPages.jsx와 연결해줌

---
+++ deco/src/components/LoginPage/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Login.module.css'
+
+const Login = () => {
+  return (
+    <div className={styles.layout}>
+      login
+    </div>
+  )
+}
+
+export default Login;
