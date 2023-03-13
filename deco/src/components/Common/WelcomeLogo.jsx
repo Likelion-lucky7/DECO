a442600c6ed904b2e19080ed51fb6e4fa10e2de3
Jayoung Hong <gripix324@gmail.com>
2023-03-13 13:42:17 +0900

    [Setting] prottierrc.json으로 변경 (적용 안 되던 거 고침)

--- deco/src/components/Common/WelcomeLogo.jsx
+++ deco/src/components/Common/WelcomeLogo.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const WelcomeLogo = () => {
-  return (
-    <div>
-      Logo
-    </div>
-  )
-}
+  return <div>Logo</div>;
+};
 
-export default WelcomeLogo
+export default WelcomeLogo;
