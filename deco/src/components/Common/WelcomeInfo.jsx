323896fb3740289d6b1a9f80a1635c5bcab0b9e9
Jayoung Hong <gripix324@gmail.com>
2023-03-13 16:29:25 +0900

    [Add] WelcomeInfo 컴포넌트 제작 (미완성)

--- deco/src/components/Common/WelcomeInfo.jsx
+++ deco/src/components/Common/WelcomeInfo.jsx
@@ -5,12 +5,12 @@ import { ReactComponent as Logo } from "../../assets/DECO_Logo.svg";
 const WelcomeLogo = () => {
   return (
     <div className={styles.layout}>
-      <Logo />
+      <Logo className={styles.logo} />
       <p className={styles.title}>
         <span className={styles.accent_primary}>DE</span>
         <span className={styles.accent_third}>CO</span>에 오신 것을 환영합니다.
       </p>
-      <p className={styles}>많은 정보를 얻어가세요 !</p>
+      <p className={styles.subtitle}>많은 정보를 얻어가세요 !</p>
     </div>
   );
 };
--- deco/src/components/LoginPage/Login.jsx
+++ deco/src/components/LoginPage/Login.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import WelcomeInfo from "@/components/Common/WelcomeInfo";
+import FormInput from "@/components/Common/FormInput";
 import styles from "./Login.module.css";
 
 const Login = () => {
   return (
     <div className={styles.layout}>
       <WelcomeInfo />
+      <FormInput />
+      <FormInput />
     </div>
   );
 };
