8e42300f4bc4c2ec9415c1dae43771b2fcad9374
semyeong park <qkrtpaud01@gmail.com>
2023-03-13 23:08:26 +0900

    [Feat] 묻고답하기 로그인 했을 때 본문 마크업 #52

--- deco/src/components/QuestionPage/QuestionDetail.jsx
+++ deco/src/components/QuestionPage/QuestionDetail.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
+import styles from "./QuestionDetail.module.css";
+import { ReactComponent as Like } from "../../assets/DECO_Like.svg";
+import emptyPicture from "../../assets/empty_picture.png";
+
+const { container, topic, profileImege, nickName, like, tagBox } = styles;
 
 const DetailPage = () => {
-  let id = useParams()
-  console.log(id)
+  let id = useParams();
+  console.log(id);
   return (
-    <div>DetailPage</div>
-  )
-}
+    <div className={container}>
+      <span className={topic}>기술</span>
+      <h2>redux와 recoil의 차이가 무엇인가요?</h2>
+      <img src={emptyPicture} className={profileImege} alt="exampleImage1" />
+      <span className={nickName}>닉네임</span>
+      <p>
+        redux와 recoil이 상태를 관리하기 위한 라이브러리로 알고 있는데 둘의
+        차이점 장단점이 궁금합니다.redux와 recoil이 상태를 관리하기 위한
+        라이브러리로 알고 있는데 둘의 차이점 장단점이 궁금합니다.redux와
+        recoil이 상태를 관리하기 위한 라이브러리로 알고 있는데 둘의 차이점
+        장단점이 궁금합니다.redux와 recoil이 상태를 관리하기 위한 라이브러리로
+        알고 있는데 둘의 차이점 장단점이 궁금합니다.redux와 recoil이 상태를
+        관리하기 위한 라이브러리로 알고 있는데 둘의 차이점 장단점이
+        궁금합니다.redux와 recoil이 상태를 관리하기 위한 라이브러리로 알고
+        있는데 둘의 차이점 장단점이 궁금합니다.redux와 recoil이 상태를 관리하기
+        위한 라이브러리로 알고 있는데 둘의 차이점 장단점이 궁금합니다.
+      </p>
+      <img src={emptyPicture} width="100" height="100" alt="exampleImage2" />
+      <img src={emptyPicture} width="100" height="100" alt="exampleImage3" />
+      <div className={tagBox}>
+        <span>#React</span>
+        <span>#React</span>
+        <span>#React</span>
+      </div>
+      <Like className={like} />
+    </div>
+  );
+};
 
 export default DetailPage;
