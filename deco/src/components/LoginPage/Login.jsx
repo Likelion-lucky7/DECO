6efd86176da4a97ff54e5e9e53f469931a676b1e
Jayoung Hong <gripix324@gmail.com>
2023-03-14 22:08:26 +0900

    [Add] 로그인 페이지에서 회원가입으로 이동하는 기능 - Link 사용

--- deco/src/components/LoginPage/Login.jsx
+++ deco/src/components/LoginPage/Login.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import WelcomeInfo from "@/components/Common/WelcomeInfo/WelcomeInfo";
 import FormInput from "@/components/Common/FormInput/FormInput";
 import SubmitButton from "@/components/Common/SubmitButton/SubmitButton";
-import classes from "./Login.module.css";
+import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className={classes.container}>
+    <div className={styles.container}>
       <WelcomeInfo subtitle="많은 정보를 얻어가세요 !" />
 
-      <form className={classes.form}>
+      <form className={styles.form}>
         <FormInput name="id" label="아이디" placeholder="아이디 입력" />
         <FormInput
           name="password"
@@ -21,9 +21,9 @@ const Login = () => {
         <SubmitButton props="로그인" />
       </form>
 
-      <p className={classes.info}>
+      <p className={styles.info}>
         아직 회원이 아니시라면 {""}
-        <Link to="./" className={classes.toSignUp}>
+        <Link to="../signup" className={styles.toSignUp}>
           회원가입
         </Link>
       </p>
