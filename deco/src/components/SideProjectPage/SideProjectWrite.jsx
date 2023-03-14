c75b1f918eec7887ef9db7dd8062a6c72dce2eb4
lulla-by <thfl3363@hanmail.net>
2023-03-15 01:47:35 +0900

    [Style] 게시글 작성페이지 인풋 커스텀 #58 - radio, checkbox, date, file 등 input 커스텀 완료 - 드롭박스 커스텀 수정 예정

--- deco/src/components/SideProjectPage/SideProjectWrite.jsx
+++ deco/src/components/SideProjectPage/SideProjectWrite.jsx
@@ -1,33 +1,55 @@
 import React from "react";
 import styles from "./SideProjectWrite.module.css";
-
+import { ReactComponent as ReactIcon } from "../../assets/sideproject/react.svg";
+import { ReactComponent as JavascriptIcon } from "../../assets/sideproject/js.svg";
+import { ReactComponent as TypescriptIcon } from "../../assets/sideproject/ts.svg";
+import { ReactComponent as FigmaIcon } from "../../assets/sideproject/figma.svg";
 const SideProjectWrite = () => {
   return (
-    <div className={styles.container}>
-      <input type="text"className={styles.titleInput} />
-      <h2>프로젝트 소개</h2>
-      <textarea name="" id="" cols="30" rows="10" style={styles.descriptionInput}/>
-      <input type="file" />
-      <h2 className={styles.date}>모집 마감</h2>
-      <input type="date" />
-      <h2 className={styles.people}>모집 인원</h2>
-      <div className={styles.dropdown}>
-        <button className={styles.dropButton}>Dropdown</button>
-        <div className={styles.dropdownContent}>
-          <a href="#">1명</a>
-          <a href="#">2명</a>
-          <a href="#">3명</a>
+    <form className={styles.container}>
+      <input type="text" className={styles.titleInput} placeholder="제목을 입력해주세요."/>
+      <div className={styles.descriptionBox}>
+        <h2 className={styles.description}>프로젝트 소개</h2>
+        <textarea
+          // name="description"
+          // defaultValue="This is a description."
+          id=""
+          cols="30"
+          rows="10"
+          className={styles.descriptionInput}
+          placeholder="궁금한 내용을 적어주세요.&#13;&#10;질문하는 내용이 구체적일수록 더 정확한 답변을 받을 수 있어오"
+        ></textarea>
+      </div>
+      <label htmlFor="input-file"  className={styles.inputButton} >업로드</label>
+      <input type="file"  id="input-file" className={styles.uploadInput} />
+
+      <div className={styles.dateBox}>
+        <h2 className={styles.date}>모집 마감</h2>
+        <input className={styles.dateInput} type="date" data-placeholder="날짜를 선택하세요" required />
+      </div>
+
+      <div className={styles.peopleBox}>
+        <h2 className={styles.people}>모집 인원</h2>
+        <div className={styles.dropdown}>
+          <button className={styles.dropButton}>인원</button>
+          <div className={styles.dropdownContent}>
+            <a href="#">1명</a>
+            <a href="#">2명</a>
+            <a href="#">3명</a>
+          </div>
         </div>
       </div>
-      <div className={styles.satus}>
-      <h2 >진행현황</h2>
-      <input type="radio" name="status" id="ing" />
-      <label htmlFor="ing">진행중</label>
-      <input type="radio" name="status" id="start" />
-      <label htmlFor="start">초기모집</label>
+
+      <div className={styles.satusBox}>
+        <h2 className={styles.status}>진행현황</h2>
+        <input type="radio" name="status" id="ing" />
+        <label htmlFor="ing">진행중</label>
+        <input type="radio" name="status" id="start" />
+        <label htmlFor="start">초기모집</label>
       </div>
-      <div className={styles.period}>
-        <h2>목표 기간</h2>
+
+      <div className={styles.periodBox}>
+        <h2 className={styles.period}>목표 기간</h2>
         <input type="radio" name="period" id="2week"></input>
         <label htmlFor="2week">1주 미만</label>
         <input type="radio" name="period" id="1month"></input>
@@ -35,8 +57,9 @@ const SideProjectWrite = () => {
         <input type="radio" name="period" id="more_than_1month"></input>
         <label htmlFor="more_than_1month">한달 이상</label>
       </div>
-      <div className={styles.role}>
-        <h2>모집 역할</h2>
+
+      <div className={styles.roleBox}>
+        <h2 className={styles.role}>모집 역할</h2>
         <input type="checkbox" id="frontend" />
         <label htmlFor="frontend">프론트엔드</label>
         <input type="checkbox" id="backend" />
@@ -49,24 +72,34 @@ const SideProjectWrite = () => {
         <label htmlFor="etc">기타</label>
       </div>
 
-      <div className={styles.skill}>
-        <h2>사용기술</h2>
+      <div className={styles.skillBox}>
+        <h2 className={styles.skill}>사용기술</h2>
         <input type="checkbox" id="javascript" />
-        <label htmlFor="javascript">자바스크립트</label>
+        <label htmlFor="javascript" aria-label="자바스크립트">
+          <JavascriptIcon className={styles.skillIcon} />
+        </label>
         <input type="checkbox" id="typescript" />
-        <label htmlFor="typescript">타입스크립트</label>
+        <label htmlFor="typescript" aria-label="타입스크립트">
+          <TypescriptIcon className={styles.skillIcon} />
+        </label>
         <input type="checkbox" id="react" />
-        <label htmlFor="react">리액트</label>
+        <label htmlFor="react" aria-label="리액트">
+          <ReactIcon className={styles.skillIcon} />
+        </label>
         <input type="checkbox" id="figma" />
-        <label htmlFor="figma">피그마</label>
+        <label htmlFor="figma" aria-label="피그마">
+          <FigmaIcon className={styles.skillIcon} />
+        </label>
       </div>
-
-      <h2>이메일 공개여부</h2>
-      <input type="radio" name="email" id="open" />
-      <label htmlFor="open">공개</label>
-      <input type="radio" name="email" id="private" />
-      <label htmlFor="private">비공개</label>
-    </div>
+      <div className={styles.emailBox}>
+        <h2 className={styles.email}>이메일 공개여부</h2>
+        <input type="radio" name="email" id="open" />
+        <label htmlFor="open">공개</label>
+        <input type="radio" name="email" id="private" />
+        <label htmlFor="private">비공개</label>
+      </div>
+      <button className={styles.submitButton}>등록</button>
+    </form>
   );
 };
 
