f14c310bb2a6fc763c1f313e599e28a3f7422dc7
hayoenn2 <dwd1205@naver.com>
2023-03-15 16:48:47 +0900

    [Refactor] 바로가기 리스트 코드 리팩토링 - 리스트 렌더링 / 조건부 렌더링 통해 리팩토링 #23

--- deco/src/components/MainPage/ShortcutList/ShortcutList.jsx
+++ deco/src/components/MainPage/ShortcutList/ShortcutList.jsx
@@ -4,41 +4,29 @@ import { ReactComponent as QuestionIcon } from "@/assets/main/question_icon.svg"
 import { ReactComponent as CommunityIcon } from "@/assets/main/community_icon.svg";
 import { ReactComponent as SideProjectIcon } from "@/assets/main/sideproject_icon.svg";
 
-const MainPage = () => {
+const MainPage = ({ list, ...restProps }) => {
+  function renderIcon(props) {
+    if (props.id === "question") {
+      return <QuestionIcon className={styles.iconBox} />;
+    } else if (props.id === "community") {
+      return <CommunityIcon className={styles.iconBox} />;
+    } else {
+      return <SideProjectIcon className={styles.iconBox} />;
+    }
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.listWrap}>
-        <QuestionIcon className={styles.iconBox} />
-        <h3 className={styles.title}>묻고 답하기</h3>
-        <p className={styles.description}>
-          더이상 영어를 번역할 일은 없습니다.
-          <br />
-          한국말로 질문을 주고 받으세요.
-        </p>
-        <ShortcutButton to="/question" />
-      </div>
-
-      <div className={styles.listWrap}>
-        <CommunityIcon className={styles.iconBox} />
-        <h3 className={styles.title}>커뮤니티</h3>
-        <p className={styles.description}>
-          다양한 사람들을 만나고
-          <br />
-          생각의 폭을 넓혀보세요.
-        </p>
-        <ShortcutButton to="/community" />
-      </div>
-
-      <div className={styles.listWrap}>
-        <SideProjectIcon className={styles.iconBox} />
-        <h3 className={styles.title}>사이드 프로젝트</h3>
-        <p className={styles.description}>
-          나에게 맞는 프로젝트나
-          <br />
-          스터디/모임을 찾아 참여해보세요.
-        </p>
-        <ShortcutButton to="sideproject" />
-      </div>
+    <div>
+      <ul className={styles.container}>
+        {list.map((item) => (
+          <li key={item.id} className={styles.listWrap}>
+            {renderIcon(item)}
+            <h3 className={styles.title}>{item.title}</h3>
+            <p className={styles.description}>{item.description}</p>
+            <ShortcutButton to={item.to} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
