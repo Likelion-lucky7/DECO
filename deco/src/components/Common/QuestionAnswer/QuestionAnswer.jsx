b9ecbc9540c2bc912626ee90bc6564c094416bb6
Jayoung Hong <gripix324@gmail.com>
2023-03-17 14:32:08 +0900

    [Feat] 묻고 답하기 상세 페이지 댓글 post 기능 구현

--- deco/src/components/Common/QuestionAnswer/QuestionAnswer.jsx
+++ deco/src/components/Common/QuestionAnswer/QuestionAnswer.jsx
@@ -1,16 +1,43 @@
-import React from "react";
+import axios from "axios";
 import Styles from "@/components/Common/QuestionAnswer/QuestionAnswer.module.css";
-import SubmitButton from "../SubmitButton/SubmitButton";
+import SubmitButton from "@/components/Common/SubmitButton/SubmitButton";
 
-// const [] = Styles;
+/* 텍스트 지우는 함수 */
+function clearText(target) {
+  target.value = "";
+}
 
 const QuestionAnswer = () => {
+  const submitData = async () => {
+    const commentWriteField = document.getElementById("comment");
+
+    await axios
+      .post(`http://localhost:3001/questionComment/`, {
+        nickname: "",
+        content: commentWriteField.value,
+      })
+      .then((res) => {
+        console.log(res);
+        clearText(commentWriteField);
+        // window.location.reload(); 자동 리로드
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className={Styles.answerWrite}>
       <h2>답변하기</h2>
-      <textarea placeholder="질문에 대한 답변을 하려면 로그인을 해주세요"></textarea>
+
+      <textarea
+        id="comment"
+        name="comment"
+        placeholder="질문에 대한 답변을 하려면 로그인을 해주세요"
+      ></textarea>
+
       <div className={Styles.submitButton}>
-        <SubmitButton title="등록" writeButton={true} />
+        <SubmitButton onClick={submitData} title="등록" writeButton={true} />
       </div>
     </div>
   );
