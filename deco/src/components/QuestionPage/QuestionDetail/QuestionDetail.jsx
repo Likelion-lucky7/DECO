0ad92fdaf7980a72e852a3077846a878e7f70677
lulla-by <thfl3363@hanmail.net>
2023-03-20 11:38:55 +0900

    [bugfix] test 브랜치 pull이후 충돌 수정 #14 - test 브랜치 pull이후 발생한 충돌 수정

--- deco/src/components/QuestionPage/QuestionDetail/QuestionDetail.jsx
+++ deco/src/components/QuestionPage/QuestionDetail/QuestionDetail.jsx
@@ -4,6 +4,7 @@ import styles from "./QuestionDetail.module.css";
 import Like from "@/assets/likeActivate.svg";
 import Comment from "@/components/Common/Comment/Comment";
 import { ReactComponent as Profile } from "@/assets/profile.svg";
+// import { ReactComponent as lik } from "@/assets/profile.svg";
 import axios from "axios";
 
 const {
@@ -24,16 +25,14 @@ const DetailPage = () => {
   let id = useParams();
   const navigate = useNavigate();
   useEffect(() => {
-    localStorage.setItem("id","user1")
+    localStorage.setItem("id", "user1");
     getData("get");
   }, []);
 
   // 게시글 조회 구현
   const getData = async () => {
     try {
-      let response = await axios.get(
-        `http://localhost:3001/question/${id.id}`,
-      );
+      let response = await axios.get(`http://localhost:3001/question/${id.id}`);
       let data = await response.data;
       setData(data);
     } catch (error) {
@@ -43,50 +42,31 @@ const DetailPage = () => {
   };
   let [data, setData] = useState([]);
 
-  let {
-    title,
-    content,
-    image,
-    user,
-    category,
-    hashTag,
-    like,
-    hits
-  } = data;
-
-
+  let { title, content, image, user, category, hashTag, like, hits } = data;
 
   return (
     <div className={container}>
-        <div>
-          <span className={topic}>{category}</span>
-          <h2 className={textTitle}>{title}</h2>
-          {user?.image === "" ? (
-            <Profile className={profileImege} />
-          ) : (
-            <img
-              src={user?.image}
-              className={profileImege}
-              alt="exampleImage1"
-            />
-          )}
-          <span className={nickName}>{user?.nickname}</span>
-          <p className={mainText}>{content}</p>
-          <img  
-          src={image}
-                className={uploadImage}
-                alt=""
-              />
-          <div className={tagBox}>
-            {hashTag?.map((item, index) => {
-              return <span key={index}>#{item}</span>;
-            })}
-          </div>
-          
+      <div>
+        <span className={topic}>{category}</span>
+        <h2 className={textTitle}>{title}</h2>
+        {user?.image === "" ? (
+          <Profile className={profileImege} />
+        ) : (
+          <img src={user?.image} className={profileImege} alt="exampleImage1" />
+        )}
+        <span className={nickName}>{user?.nickname}</span>
+        <p className={mainText}>{content}</p>
+        <img src={image} className={uploadImage} alt="" />
+        <div className={tagBox}>
+          {hashTag?.map((item, index) => {
+            return <span key={index}>#{item}</span>;
+          })}
         </div>
-     
+      </div>
+
       <button className={styles.likeIcon}>
         <img src={Like} alt="하트" />
+        <span>좋아요</span>
       </button>
       <Comment />
     </div>
