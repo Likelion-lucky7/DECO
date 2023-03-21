941ce0fef43221648f596b5d216a3becfabe5006
lulla-by <thfl3363@hanmail.net>
2023-03-22 01:14:56 +0900

    [Feat] 묻고답하기 게시글 수정 기능 #22 - 게시글을 작성한 유저만 수정 가능 - 게시글의 title과 content 수정 기능

--- deco/src/components/QuestionPage/QuestionDetail/QuestionDetail.jsx
+++ deco/src/components/QuestionPage/QuestionDetail/QuestionDetail.jsx
@@ -5,7 +5,8 @@ import Like from "@/assets/heartActivate.svg";
 import Comment from "@/components/Common/Comment/Comment";
 import { ReactComponent as Profile } from "@/assets/profile.svg";
 import { dbService } from "@/firebase/app";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, updateDoc, doc, deleteDoc } from "firebase/firestore";
+import styles2 from "@/components/Common/WriteInput/WriteInput.module.css";
 
 const {
   container,
@@ -21,8 +22,14 @@ const {
 
 const DetailPage = () => {
   let id = useParams();
+  let navigate = useNavigate()
   let [data, setData] = useState([]);
-
+  
+  let [editMode, setEditMode] = useState(false);
+  let [ubdateTitle, setUpdateTitle] = useState("");
+  let [ubdateContent, setUpdateContent] = useState("");
+  
+  // 게시글 조회
   const getNweets = async () => {
     const querySnapShot = await getDocs(collection(dbService, "nweets"));
     querySnapShot.forEach((doc) => {
@@ -33,13 +40,82 @@ const DetailPage = () => {
   };
   useEffect(() => {
     getNweets();
+    localStorage.setItem("id","user1")
   }, []);
 
   let { title, content, image, user, category, hashTag, like, hits } = data;
+// console.log(user?.nickname)
+
+
+// 게시글 수정
+
+const onChange = (e) =>{
+  const {target:{value,name}}= e;
+  if(name==="title"){
+    setUpdateTitle(value)
+  } else if(name==="content"){
+    setUpdateContent(value)
+  }
+}
+
+const onUpdate = async (e) => {
+  e.preventDefault()
+  // 임의의 데이터 path를 지정 추후 로그인 인증후 변경 필요
+  let testPath = "YOm6tv5go7u9MJn4zRdB"
+  const ok = window.confirm("수정하시겠습니까")
+  if(ok){
+    await updateDoc(doc(dbService, "nweets", testPath),{title:ubdateTitle, content:ubdateContent});
+    setEditMode(!editMode)
+  }else{
+    console.log("취소")
+  }
+};
+
+
+
+
 
   return (
     <div className={container}>
-      <div>
+  {  editMode ? <div>
+          <form onSubmit={onUpdate}>
+          <input
+            id={id}
+            type="text"
+            name="title"
+            placeholder="제목을 입력해주세요."
+            className={styles2.title}
+            onChange={onChange}
+            required
+          />
+          <span className={styles2.totalNumber}>0 / 100</span>
+
+          <textarea
+            name="content"
+            id="content"
+            cols="30"
+            rows="10"
+            placeholder="궁금한 내용을 적어주세요.&#10;질문하는 내용이 구체적일수록 더 정확한 답변을 받을 수 있어요."
+            className={styles2.content}
+            onChange={onChange}
+            required
+          ></textarea>
+          <button
+            title=""
+          >
+            수정
+          </button>
+        </form>
+          <button
+            onClick={() => {
+              setEditMode(false)
+            }}
+            title="취소"
+          >
+            취소
+          </button>
+        </div>
+ :  <div>
         <span className={topic}>{category}</span>
         <h2 className={textTitle}>{title}</h2>
         {user?.profile === "" ? (
@@ -59,12 +135,19 @@ const DetailPage = () => {
             return <span key={index}>#{item}</span>;
           }):null}
         </div>
-      </div>
+      </div>}
 
       <button className={styles.likeIcon}>
         <img src={Like} alt="하트" />
         <span>좋아요</span>
       </button>
+      {localStorage.getItem("id") == user?.nickname && editMode === false ? (
+        <>
+        <button >삭제</button>
+        <button onClick={()=>{setEditMode(true)}}>수정</button>
+        </>
+      ) : null}
+
       <Comment />
     </div>
   );
