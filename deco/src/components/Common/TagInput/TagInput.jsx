00a572b227c64f76c3b934cd016a628350c69efa
hayoenn2 <dwd1205@naver.com>
2023-03-22 16:31:02 +0900

    [Feat] 작성란 해시태그 기능 추가 #91

--- deco/src/components/Common/TagInput/TagInput.jsx
+++ deco/src/components/Common/TagInput/TagInput.jsx
@@ -2,29 +2,70 @@ import { bool, string } from "prop-types";
 import styles from "./TagInput.module.css";
 import { useRecoilState } from "recoil";
 import { hashTagState } from "@/@store/hashTagState";
+import { useState } from "react";
 
 const TagInput = ({ isQuestion }) => {
-  const [hashTag, setHashTag] = useRecoilState(hashTagState);
+  const [tagItem, setTagItem] = useState("");
+  const [tagList, setTagList] = useState([]);
+
   const onChangeTag = (e) => {
-    setHashTag(e.target.value);
+    setTagItem(e.target.value);
   };
+
+  const onKeyPress = (e) => {
+    if (e.target.value.length !== 0 && e.key === "Enter") {
+      submitTagItem();
+    }
+  };
+
+  const submitTagItem = () => {
+    let updatedTagList = [...tagList];
+    console.log(tagList);
+    updatedTagList.push(tagItem);
+    setTagList(updatedTagList);
+    setTagItem("");
+  };
+
+  const deleteTagItem = (e) => {
+    const deleteTagItem = e.target.parentElement.firstChild.innerText;
+    const filteredTagList = tagList.filter(
+      (tagItem) => tagItem !== deleteTagItem,
+    );
+    setTagList(filteredTagList);
+  };
+
   return (
-    <div>
-      {isQuestion ? (
-        <input
-          type="text"
-          placeholder="태그 1개 이상 필수 입력 (예: #react, #javascript 등 5개까지)"
-          className={styles.tag_question}
-          onChange={onChangeTag}
-          value={hashTag}
-        />
-      ) : (
-        <input
-          type="text"
-          placeholder="태그 1개 이상 필수 입력 (예: #react, #javascript 등 5개까지)"
-          className={styles.tag}
-        />
-      )}
+    <div className={styles.box}>
+      {tagList.map((hashTag, idx) => {
+        return (
+          <div key={idx} className={styles.item}>
+            <span>{hashTag}</span>
+            <button onClick={deleteTagItem} className={styles.hashButton}>
+              ✕
+            </button>
+          </div>
+        );
+      })}
+
+      <div className={styles.inputWrapper}>
+        <span className={styles.hash}>#</span>
+        {isQuestion ? (
+          <input
+            type="text"
+            placeholder="태그 1개 이상 필수 입력 (예: #react, #javascript 등)"
+            className={styles.tag_question}
+            onChange={(e) => setTagItem(e.target.value)}
+            value={tagItem}
+            onKeyUp={onKeyPress}
+          />
+        ) : (
+          <input
+            type="text"
+            placeholder="태그 1개 이상 필수 입력 (예: #react, #javascript 등)"
+            className={styles.tag}
+          />
+        )}
+      </div>
     </div>
   );
 };
