90a7a990d71f9c0e4e488c30f956e8a1f1b3bad4
hayoenn2 <dwd1205@naver.com>
2023-03-22 16:30:26 +0900

    [Feat] 묻고답하기 작성 페이지에 Recoil 추가 #91

--- deco/src/components/QuestionPage/QuestionWrite/QuestionWrite.jsx
+++ deco/src/components/QuestionPage/QuestionWrite/QuestionWrite.jsx
@@ -3,8 +3,32 @@ import FileUpload from "@/components/Common/FileUpload/FileUpload";
 import TagInput from "@/components/Common/TagInput/TagInput";
 import styles from "./QuestionWrite.module.css";
 import SubmitButton from "@/components/Common/SubmitButton/SubmitButton";
+import { useRecoilValue } from "recoil";
+import { titleState } from "@/@store/titleState";
+import { collection, addDoc } from "firebase/firestore";
+import { dbService } from "@/firebase/app";
+import { contentState } from "@/@store/contentState";
+import { hashTagState } from "@/@store/hashTagState";
 
 const QuestionWrite = () => {
+  const inputTitle = useRecoilValue(titleState);
+  const inputContent = useRecoilValue(contentState);
+  const inputHashTag = useRecoilValue(hashTagState);
+
+  const submitTitle = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "question"), {
+        title: inputTitle,
+        content: inputContent,
+        hashtag: inputHashTag,
+      });
+      console.log("성공?", docRef.id);
+    } catch (e) {
+      console.error("error");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.info}>
@@ -40,10 +64,9 @@ const QuestionWrite = () => {
         <TagInput isQuestion={true} />
         <div className={styles.rowButton}>
           <FileUpload isSignUp={false} />
-          <SubmitButton title="등록" writeButton={true} />
+          <SubmitButton onClick={submitTitle} title="등록" writeButton={true} />
         </div>
       </div>
-      {/* <button onClick={handleTest}>클릭</button> */}
     </div>
   );
 };
