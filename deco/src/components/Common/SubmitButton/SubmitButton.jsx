18639ecea01d846e7fb7cab744cdbb519da1b819
hayoenn2 <dwd1205@naver.com>
2023-03-23 16:11:51 +0900

    [Style] SubmitButton 컴포넌트 수정 #99

--- deco/src/components/Common/SubmitButton/SubmitButton.jsx
+++ deco/src/components/Common/SubmitButton/SubmitButton.jsx
@@ -1,20 +1,17 @@
-import { useId } from "react";
 import styles from "./SubmitButton.module.css";
 
 const SubmitButton = ({ writeButton, title, ...restProps }) => {
-  const id = useId();
-
   return (
     <>
       {writeButton ? (
         <div className={styles.container_writeButton}>
-          <button id={id} className={styles.button_writeButton} {...restProps}>
+          <button className={styles.button_writeButton} {...restProps}>
             {title}
           </button>
         </div>
       ) : (
         <div className={styles.container}>
-          <button id={id} className={styles.button} {...restProps}>
+          <button className={styles.button} {...restProps}>
             {title}
           </button>
         </div>
