c613d7febaa3bc89dcf03f91b1fff827547c00c9
hayoenn2 <dwd1205@naver.com>
2023-03-24 10:24:09 +0900

    [Feat] 이미지 2개 업로드 되도록 구현 #99

--- deco/src/components/Common/FileUpload/FileUpload.jsx
+++ deco/src/components/Common/FileUpload/FileUpload.jsx
@@ -1,7 +1,31 @@
 import { ReactComponent as Upload } from "../../../assets/file_upload.svg";
 import styles from "./FileUpload.module.css";
+import { useState, useRef, useEffect } from "react";
 
 const FileUpload = ({ isSignUp }) => {
+  const [imgFile, setImgFile] = useState([]);
+  const imgRef = useRef([null, null]);
+  const [isVisible, setIsVisible] = useState([true, false]);
+
+  // 이미지 업로드 input의 onChange
+  const saveImgFile = (index) => {
+    const file = imgRef.current[index].files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      const previewImgUrl = reader.result;
+      if (previewImgUrl) {
+        setImgFile([...imgFile, previewImgUrl]);
+      }
+    };
+  };
+
+  useEffect(() => {
+    if (imgFile[0]) {
+      setIsVisible([true, true]);
+    }
+  }, [imgFile]);
+
   return (
     <div>
       {isSignUp ? (
@@ -19,18 +43,42 @@ const FileUpload = ({ isSignUp }) => {
           />
         </div>
       ) : (
-        <div>
-          <label htmlFor="file" className={styles.profile_label}>
-            <Upload className={styles.profile_image} /> 파일 업로드
-          </label>
-
-          <input
-            id="file"
-            type="file"
-            accept="image/jpg, image/png, image/jpeg"
-            name="profile"
-            className={styles.profile_form}
-          />
+        <div className={styles.box}>
+          {isVisible.map((item, index) => {
+            {
+              index;
+            }
+            return item ? (
+              <div key={index}>
+                <label htmlFor="file" className={styles.profile_label}>
+                  <span className={styles.profile_image}>
+                    {!imgFile[index] && `파일 업로드`}
+                    {imgFile[index] && (
+                      <img
+                        src={
+                          imgFile[index]
+                            ? imgFile[index]
+                            : `/images/icon/user.png`
+                        }
+                        alt="프로필 이미지"
+                      />
+                    )}
+                  </span>
+                </label>
+                <input
+                  ref={(el) => {
+                    imgRef.current[index] = el;
+                  }}
+                  id="file"
+                  type="file"
+                  accept="image/jpg, image/png, image/jpeg"
+                  name="profile"
+                  className={styles.profile_form}
+                  onChange={() => saveImgFile(index)}
+                />
+              </div>
+            ) : null;
+          })}
         </div>
       )}
     </div>
