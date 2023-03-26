7b5c2067c85e341343385b42326ec2aa8003539d
lulla-by <thfl3363@hanmail.net>
2023-03-26 22:01:15 +0900

    [Style] 묻고 답하기 카테고리 css수정 #12 - 카테고리 div에서 button으로 수정 - css수정

--- deco/src/components/Common/Category/Category.jsx
+++ deco/src/components/Common/Category/Category.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Category.module.css";
 
-const Category = ({ category1 = "", category2 = "", path = "" }) => {
+const Category = ({ category1 = "", category2 = "", path = "",...restprps }) => {
+
   return (
     <div className={styles.container}>
       <div className={styles.inner}>
-        <Link to={path} href={path}>
+        <button to={path} href={path} name="all" {...restprps} >
           전체
-        </Link>
-        <Link to="/" href="/">
+        </button>
+        <button to="/" href="/" name={category1} {...restprps} >
           {category1}
-        </Link>
-        <Link to="/" href="/">
+        </button>
+        <button to="/" href="/" name={category2} {...restprps} >
           {category2}
-        </Link>
+        </button>
       </div>
     </div>
   );
