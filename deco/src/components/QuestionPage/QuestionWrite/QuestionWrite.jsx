53243514dab04019373e0fe99b3110b6ac5cd8d9
hayoenn2 <dwd1205@naver.com>
2023-03-27 18:31:11 +0900

    [Feat] createdAt 사용해서 실시간 날짜 전송 #118

--- deco/src/components/QuestionPage/QuestionWrite/QuestionWrite.jsx
+++ deco/src/components/QuestionPage/QuestionWrite/QuestionWrite.jsx
@@ -58,6 +58,7 @@ const QuestionWrite = () => {
         image: inputFileImage,
         id: "",
         date: "",
+        createdAt: Date.now(),
         hits: 0,
         like: 0,
         user: {
