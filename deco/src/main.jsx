17c0b70ea97748830105864ba19c8e6f89fd2fe6
Donggyeong Yi <realldk@naver.com>
2023-03-28 14:39:42 +0900

    [Setting] BrowerRouter를 HashRouter로 바꾸고 basename="/" 추가설정

--- deco/src/main.jsx
+++ deco/src/main.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./app/App";
 import "./styles/index.css";
 
-const { VITE_PUBLIC_URL } = import.meta.env;
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={VITE_PUBLIC_URL}>
+    <HashRouter basename="/">
       <RecoilRoot>
         <App />
       </RecoilRoot>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
