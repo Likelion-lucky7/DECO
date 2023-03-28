ee7702d08a4b51fc5b506ba31b8be7fff857b729
Donggyeong Yi <realldk@naver.com>
2023-03-28 14:14:09 +0900

    [Setting] base경로 수정

--- deco/vite.config.js
+++ deco/vite.config.js
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  base: "/DECO/",
+  base: "",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
