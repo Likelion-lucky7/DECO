57de380049a3fac93335aa0d636c45ee480a856a
Donggyeong Yi <realldk@naver.com>
2023-03-28 14:29:42 +0900

    [Setting] browserRouter basename 설정

--- deco/src/main.jsx
+++ deco/src/main.jsx
@@ -5,9 +5,11 @@ import { RecoilRoot } from "recoil";
 import App from "./app/App";
 import "./styles/index.css";
 
+const { VITE_PUBLIC_URL } = import.meta.env;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={VITE_PUBLIC_URL}>
       <RecoilRoot>
         <App />
       </RecoilRoot>
