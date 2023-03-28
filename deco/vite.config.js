251baae85dd809358ec026eedd55719a446c5c59
Donggyeong Yi <realldk@naver.com>
2023-03-28 13:19:55 +0900

    [Deploy] vite.config base 설정

--- deco/vite.config.js
+++ deco/vite.config.js
@@ -6,6 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  base: "/DECO/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
