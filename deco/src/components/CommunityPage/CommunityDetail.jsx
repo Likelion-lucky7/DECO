7fd10e28610230f7544feff6c252cc1311ac1617
hayoenn2 <dwd1205@naver.com>
2023-04-07 15:48:51 +0900

    [Feat] id 임의로 만들어서 커뮤니티 제목, 내용, 이미지 받아오기 #138 - 파이어베이스와 연동

--- deco/src/components/CommunityPage/CommunityDetail.jsx
+++ deco/src/components/CommunityPage/CommunityDetail.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
 import styles from "@/components/CommunityPage/CommunityDetail.module.css";
 import Like from "@/assets/heartActivate.svg";
 import Comment from "@/components/Common/Comment/Comment";
+import { getCommunity } from "@/@store/getCommunityData";
+import { useRecoilValue } from "recoil";
+import { useParams } from "react-router-dom";
+
+console.log(getCommunity);
 
 const CommunityDetail = () => {
+  let id = useParams();
+  console.log("나는 useParams 아이디여", id);
+
+  let communityData = useRecoilValue(getCommunity);
+  console.log("나는 커뮤니티 데이터!", communityData);
+
+  let data = communityData.filter((item) => item.id === id.id)[0];
+  console.log("나는 필터했지롱.", data);
+
+  let { title, content, image } = data;
+
   return (
     <div className={styles.container}>
       <div>
-        <h2 className={styles.textTitle}>제목</h2>
+        <h2 className={styles.textTitle}>{title}</h2>
         <span className={styles.nickname}>{"user"?.nickname}</span>
-        <p className={styles.mainText}>안녕안녕 컨텐츠</p>
-        <img src={"image"} className={styles.uploadImage} alt="" />
+        <p className={styles.mainText}>{content}</p>
+        {image ? (
+          <img src={image} className={styles.uploadImage} alt="" />
+        ) : null}
         <div className={styles.tagBox}></div>
       </div>
       <button className={styles.likeIcon}>
